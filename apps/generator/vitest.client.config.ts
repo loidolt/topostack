6c6cbf5bef79d5468ee2a61ca569add76127d47a
loidolt <loidolt@gmail.com>
2026-08-19 15:12:22 -0600

    Use SvelteKit aliases in browser tests

--- apps/generator/vitest.client.config.ts
+++ apps/generator/vitest.client.config.ts
@@ -1,8 +1,8 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [sveltekit()],
   resolve: { conditions: ["browser"] },
   test: {
     environment: "jsdom",
